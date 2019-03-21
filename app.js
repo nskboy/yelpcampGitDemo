932d465fd8f8fb5da682038fdab9bad0f52736e8
Smileyx96 <jasonleezhiyong@gmail.com>
2019-03-21 13:41:32 +0800

    error solve

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes = require("./routes/index");
 
-console.log(process.env.DATABASEURL);
-mongoose.connect('process.env.DATABASEURL', { useNewUrlParser: true });
+// var url = process.env.DATABASEURL || "mongodb://localhost.yelp_campv9"
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended:true}));
 mongoose.set("useFindAndModify", false);
 app.set('view engine', 'ejs');
